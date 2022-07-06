476e87777f1f3994a9ebf780406dcdb9e068eb96
caio300 <caioqs.info@gmail.com>
2022-07-06 15:34:01 -0300

    Concluído componente Comment e feito Css

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/caio300.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Caio Queiroz</strong>
+              <time title='05 de Julho às 08:13h' dateTime='2022-07-05 08:13:30'>Cerca de 1h atrás</time>
+            </div>
+            <button title='Deletar Comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
